94eea11133b355e5007aed515cdc00f8e22a3c18
Conradong <conradongoncalves@gmail.com>
2026-09-22 21:35:36 -0300

    Implementa gerenciamento de receitas em Editar Finanças

---
+++ android/app/src/main/java/com/example/radarfinanceiro/models/DespesaRequest.java
@@ -0,0 +1,30 @@
+package com.example.radarfinanceiro.models;
+
+public class DespesaRequest {
+    private String categoria;
+    private String tipo;
+    private String nomeDespesa;
+    private String descricao;
+    private double valorUnitario;
+    private double quantidade;
+    private double valorOrcado;
+    private int projetoId;
+
+    public DespesaRequest(
+            String categoria,
+            String tipo,
+            String nomeDespesa,
+            double valorUnitario,
+            double quantidade,
+            double valorOrcado,
+            int projetoId
+    ) {
+        this.categoria = categoria;
+        this.tipo = tipo;
+        this.nomeDespesa = nomeDespesa;
+        this.valorUnitario = valorUnitario;
+        this.quantidade = quantidade;
+        this.valorOrcado = valorOrcado;
+        this.projetoId = projetoId;
+    }
+}
